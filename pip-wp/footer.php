48d69079c361fc12bce4a3e60eccec46ed0c1b5d
Jason Carroll <jason@champ.net>
2015-07-20 11:39:42 -0500

    added bootstrap and jquery scripts to footer pages

--- pip-wp/footer.php
+++ pip-wp/footer.php
@@ -18,7 +18,7 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="<?php echo bloginfo('template_directory'); ?>/js/bootstrap.js"></script>
+<script src="<?php echo bloginfo('template_directory'); ?>/assets/js/bootstrap.js"></script>
 
 <script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-2.0.3.js"%3E%3C/script%3E'))</script>
 <script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/general.js"></script>
