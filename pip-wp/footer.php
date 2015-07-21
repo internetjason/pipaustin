fde2c4864992de7cb77dc73354ead6ac23ee15ed
Jason Carroll <jason@champ.net>
2015-07-21 09:32:33 -0500

    added closing div and commented out incorrect version of jquery

--- pip-wp/footer.php
+++ pip-wp/footer.php
@@ -1,15 +1,15 @@
 <?php get_sidebar(); ?>
-
-    <footer>
-        <div class="row">
-            <div class="col-sm-12 text-center small">
-                <p>&copy;<?php echo date("Y"); ?> Partners in Parenting. All Rights Reserved</p>
-                <p>Partners in Parenting is a 501(c)(3) non-profit recognized by the IRS</p>
-                <p>home | contact | faq | <a href="#top" title="Jump back to top">&#8593;</a></p>
-                <p>privacy policy</p>
+    </div>
+        <footer>
+            <div class="row">
+                <div class="col-sm-12 text-center small">
+                    <p>&copy;<?php echo date("Y"); ?> Partners in Parenting. All Rights Reserved</p>
+                    <p>Partners in Parenting is a 501(c)(3) non-profit recognized by the IRS</p>
+                    <p>home | contact | faq | <a href="#top" title="Jump back to top">&#8593;</a></p>
+                    <p>privacy policy</p>
+                </div>
             </div>
-        </div>
-    </footer>
+        </footer>
 
 </div><!-- / . body wrapper -->
 
@@ -20,7 +20,7 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/bootstrap.js"></script>
 
-<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-2.0.3.js"%3E%3C/script%3E'))</script>
+<!--<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-2.0.3.js"%3E%3C/script%3E'))</script>-->
 <script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/general.js"></script>
 <?php if ( is_singular() ) wp_print_scripts( 'comment-reply' ); ?>
 </body>
