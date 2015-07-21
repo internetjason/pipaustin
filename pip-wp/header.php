dd5a93ec209ead536448b8821097f1bdab869315
Jason Carroll <jason@champ.net>
2015-07-21 08:18:54 -0500

    removed duplicate code and fixed some header meta stuff

--- pip-wp/header.php
+++ pip-wp/header.php
@@ -1,116 +1,101 @@
 <?php ?>
-
-
-
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Favicon -->
-        <link rel="icon" type="image/png" href="img/favicon.png">
-
-        <title>Partners in Parenting</title>
-        <meta name="description" content="">
-
-
-
-
 <!DOCTYPE html>
-        <html lang="en">
 <!--[if IE 9 ]><html lang="en" class="ie ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
-<head>
-<title><?php
-	global $page, $paged;
-	wp_title( '|', true, 'right' );
-		bloginfo( 'name' );
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) )
-			echo " | $site_description";
-		if ( $paged >= 2 || $page >= 2 )
-			echo ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
-	?>
-</title>
-<meta name="description" content="<?php if ( is_single() ) {
-	single_post_title('', true);
-	} else {
-	bloginfo('name'); echo " - "; bloginfo('description');
-	}
-?>" />
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+        <head>
+            <title><?php
+global $page, $paged;
+wp_title( '|', true, 'right' );
+bloginfo( 'name' );
+$site_description = get_bloginfo( 'description', 'display' );
+if ( $site_description && ( is_home() || is_front_page() ) )
+    echo " | $site_description";
+if ( $paged >= 2 || $page >= 2 )
+    echo ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
+                ?>
+            </title>
+            <meta name="description" content="<?php if ( is_single() ) {
+                    single_post_title('', true);
+                } else {
+                    bloginfo('name'); echo " - "; bloginfo('description');
+                }
+                                              ?>" />
+            <!-- <meta charset="<?php bloginfo( 'charset' ); ?>" /> -->
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- The little things -->
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-    <link rel="shortcut icon" href="<?php echo bloginfo('template_directory'); ?>/favicon.png">
-	<link rel="apple-touch-icon" href="<?php echo bloginfo('template_directory'); ?>/apple-touch-icon-precomposed.png"/>
-<!-- The little things -->
+            <!-- Favicons -->
+            <link rel="profile" href="http://gmpg.org/xfn/11" />
+            <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+            <link rel="shortcut icon" href="<?php echo bloginfo('template_directory'); ?>/favicon.png">
+            <link rel="apple-touch-icon" href="<?php echo bloginfo('template_directory'); ?>/apple-touch-icon-precomposed.png"/>
+            <!-- The little things -->
 
-<!-- Stylesheets -->
+            <!-- Stylesheets -->
 
-    <!-- Bootstrap -->
-    <link href="<?php echo bloginfo('template_directory'); ?>/assets/css/bootstrap.css" rel="stylesheet">
+            <!-- Bootstrap -->
+            <link href="<?php echo bloginfo('template_directory'); ?>/assets/css/bootstrap.css" rel="stylesheet">
 
-    <!-- Font Awesome -->
-    <link href="<?php echo bloginfo('template_directory'); ?>/assets/css/font-awesome.css" rel="stylesheet">
+            <!-- Font Awesome -->
+            <link href="<?php echo bloginfo('template_directory'); ?>/assets/css/font-awesome.css" rel="stylesheet">
 
-    <!-- Custom Styles -->
-    <link href="<?php echo bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+            <!-- Custom Styles -->
+            <link href="<?php echo bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 
-    <!-- Custom Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
+            <!-- Custom Google Fonts -->
+            <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
 
-<!-- Stylesheets -->
+            <!-- Stylesheets -->
 
-<!--
-    Optional Stuff - Remove comment if you need it
-    <script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/modernizr-2.6.2.js"></script>
+            <!--
+Optional Stuff - Remove comment if you need it
+<script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/modernizr-2.6.2.js"></script>
 -->
-	<?php wp_deregister_script('jquery');wp_head(); ?>
+            <?php wp_deregister_script('jquery');wp_head(); ?>
 
-</head>
+        </head>
 
-<body <?php body_class(); ?> id="top">
-<!--
-    <header role="banner">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a>
-        <p class="desc">
-			<?php bloginfo( 'description' ); ?>
-        </p>
-        <nav role="navigation">
-            <?php $args = array( 'menu' => 'mainnav', 'container' => false, 'menu_id' => false, 'menu_class' => false); wp_nav_menu($args); ?>
-        </nav>
-        <?php get_search_form(); ?>
-    </header>
+        <body <?php body_class(); ?> id="top">
+            <!--
+<header role="banner">
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a>
+<p class="desc">
+<?php bloginfo( 'description' ); ?>
+</p>
+<nav role="navigation">
+<?php $args = array( 'menu' => 'mainnav', 'container' => false, 'menu_id' => false, 'menu_class' => false); wp_nav_menu($args); ?>
+</nav>
+<?php get_search_form(); ?>
+</header>
 -->
-    <!-- BODY WRAPPER -->
-    <div class="body-wrapper">
+            <!-- BODY WRAPPER -->
+            <div class="body-wrapper">
 
-        <!-- Static navbar -->
-        <div class="navbar-wrapper">
-            <nav class="navbar navbar-default navbar-static-top">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#"><img class="logo" src="<?php echo bloginfo('template_directory'); ?>/assets/img/parnters-in-parenting-austin.png"></a>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
-                        </ul>
-                        <!-- Wordpress Navbar Code -->
-                        <?php $args = array( 'menu' => 'mainnav', 'container' => false, 'menu_id' => false, 'menu_class' => 'nav navbar-nav navbar-right'); wp_nav_menu($args); ?>
-                    </div>
+                <!-- Static navbar -->
+                <div class="navbar-wrapper">
+                    <nav class="navbar navbar-default navbar-static-top">
+                        <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="#"><img class="logo" src="<?php echo bloginfo('template_directory'); ?>/assets/img/parnters-in-parenting-austin.png"></a>
+                            </div>
+                            <div id="navbar" class="navbar-collapse collapse">
+                                <ul class="nav navbar-nav">
+                                </ul>
+                                <!-- Wordpress Navbar Code -->
+                                <?php $args = array( 'menu' => 'mainnav', 'container' => false, 'menu_id' => false, 'menu_class' => 'nav navbar-nav navbar-right'); wp_nav_menu($args); ?>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
-            </nav>
-        </div>
 
-        <!-- Donate Button -->
-        <div class="container">
-            <span id="donate-button"></span>
-        </div>
+                <!-- Donate Button -->
+                <div class="container">
+                    <span id="donate-button"></span>
+                </div>
